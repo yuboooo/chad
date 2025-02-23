5ff094912d5154c8c2b5bdf042add1614537778f
Yubo Jing <yubo_jing@brown.edu>
2025-02-22 22:34:19 -0500

    try docker compose setup

--- backend/app.py
+++ backend/app.py
@@ -10,5 +10,5 @@ def hello():
     return {"message": "Hello from Chad"}
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5001))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host="0.0.0.0", port=port)
